6d7d08cab2b245f0f659c328c143e4a679e8eb6c
Simon Li <spli@dundee.ac.uk>
2020-01-06 18:38:22 +0000

    docker.sh builds docs in docs/

--- docker.sh
+++ docker.sh
@@ -1,1 +1,1 @@
-docker run -it --rm --name jekyll -p 4000:4000 -v $PWD:/src jekyll/jekyll jekyll server -w -s /src "$@"
+docker run -it --rm --name jekyll -p 4000:4000 -v $PWD:/src jekyll/jekyll jekyll build -s /src -d /src/docs "$@"
